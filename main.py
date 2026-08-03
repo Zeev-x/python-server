509b185a0d535116f798f1681209624db2e6eca5
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2026-08-03 19:28:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ def home():
     return "<h1>Reyette Server</h1>", "text/html"
 
 if __name__ == "__main__":
-    reyette_server.run(port=8080, directory=".", chunk_size=256 * 1024)
+    server.run(port=8080, directory=".", chunk_size=256 * 1024)
