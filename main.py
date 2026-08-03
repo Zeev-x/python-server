563ab8699a9d160512bf9f57d44071a48a5bff00
Reyette Roxylious Atelier <87901027+Zeev-x@users.noreply.github.com>
2026-08-03 19:26:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # main.py for example
 import server
 
-@reyette_server.route("/home")
+@server.route("/home")
 def home():
     return "<h1>Reyette Server</h1>", "text/html"
 
 if __name__ == "__main__":
-    reyette_server.run(port=8080, directory="D:\\Reyette-Downloader", chunk_size=256 * 1024)
+    reyette_server.run(port=8080, directory=".", chunk_size=256 * 1024)
